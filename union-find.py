50d7e0148fdb88a65110bbef86aa57c3f8fa8c55
Maks <maksym.doroshenko@tech.jago.com>
2022-11-24 14:27:30 +0100

    [+] Weighted Quick Union Find

--- union-find.py
+++ union-find.py
@@ -32,9 +32,36 @@ class QUF:
             return self.get_root(self.ids[node])
 
 
+class WQUF:
+    def __init__(self, size: int):
+        self.ids = list(range(0, size))
+        self.weights = [1] * size
+
+    def union(self, node1: int, node2: int):
+        node1_root = self.get_root(node1)
+        node2_root = self.get_root(node2)
+        if self.weights[node1_root] >= self.weights[node2_root]:
+            self.ids[node2_root] = node1_root
+            self.weights[node1_root] += self.weights[node2_root]
+        else:
+            self.ids[node1_root] = node2_root
+            self.weights[node2_root] += self.weights[node1_root]
+        print(self.ids)
+
+    def connected(self, node1: int, node2: int) -> bool:
+        return self.get_root(node1) == self.get_root(node2)
+
+    def get_root(self, node: int) -> int:
+        self.ids[node] = self.ids[self.ids[node]]  # path compression
+        if self.ids[node] == node:
+            return node
+        else:
+            return self.get_root(self.ids[node])
+
+
 if __name__ == '__main__':
     n = int(input("Enter number of elements:"))
-    uf = QUF(n)
+    uf = WQUF(n)
     print(uf.ids)
     print(len(uf.ids))
     connection = input("Add connections:\n")
